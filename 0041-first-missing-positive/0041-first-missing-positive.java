da95193a14529c98cd8456f3b5e5a6c64872e45a
Vedant Dwivedi <117095684+vedant-dwivedi@users.noreply.github.com>
2024-03-26 15:23:25 +0530

    Time: 1 ms (98.00%), Space: 54.9 MB (73.20%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,25 +1,28 @@
 class Solution {
-  public int firstMissingPositive(int[] nums) {
-    final int n = nums.length;
-
-    // Correct slot:
-    // nums[i] = i + 1
-    // nums[i] - 1 = i
-    // nums[nums[i] - 1] = nums[i]
-    for (int i = 0; i < n; ++i)
-      while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1])
-        swap(nums, i, nums[i] - 1);
-
-    for (int i = 0; i < n; ++i)
-      if (nums[i] != i + 1)
-        return i + 1;
-
-    return n + 1;
-  }
-
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        // Place each positive integer i at index i-1 if possible
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        // Find the first missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // If all positive integers from 1 to n are present, return n + 1
+        return n + 1;
+    }
 }
